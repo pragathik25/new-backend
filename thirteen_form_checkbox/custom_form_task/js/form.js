4a29bf2cbac3ab4688ebe359e1408e906179e706
pragathik25 <pkshetty4446@gmail.com>
2023-08-18 09:15:09 +0530

    task

--- thirteen_form_checkbox/custom_form_task/js/form.js
+++ thirteen_form_checkbox/custom_form_task/js/form.js
@@ -1,17 +1,33 @@
-(function ($, Drupal) {
-    $(document).ready(function () {
-        var $noLastName = $('#no-last-name');
-        var $lastName = $('.form-item-last-name');
-        if ($noLastName.is(':checked')) {
-            $lastName.hide();
-        }
+// (function ($, Drupal) {
+//     $(document).ready(function () {
+//         var $noLastName = $('#no-last-name');
+//         var $lastName = $('.form-item-last-name');
+//         if ($noLastName.is(':checked')) {
+//             $lastName.hide();
+//         }
 
-        $noLastName.on('change', function () {
-        if ($(this).is(':checked')) {
-            $lastName.hide();
-        } else {
-            $lastName.show();
-        }
-        });
+//         $noLastName.on('change', function () {
+//         if ($(this).is(':checked')) {
+//             $lastName.hide();
+//         } else {
+//             $lastName.show();
+//         }
+//         });
+//     });
+// }(jQuery, Drupal));
+document.addEventListener('DOMContentLoaded', function() {
+    const noLastName = document.getElementById('no-last-name');
+    const lastName = document.querySelector('.form-item-last-name');
+
+    if (noLastName.checked) {
+    lastName.style.display = 'none';
+    }
+
+    noLastName.addEventListener('change', function() {
+    if (this.checked) {
+        lastName.style.display = 'none';
+    } else {
+        lastName.style.display = 'block';
+    }
     });
-}(jQuery, Drupal));
+});
